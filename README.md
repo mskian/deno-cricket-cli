36e70dc2bab7c0b04839399484f951dbf607e2a7
mskian <10300271+mskian@users.noreply.github.com>
2022-08-04 19:42:48 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,107 @@
+# Cricket CLI
+
+[![Test CLI](https://github.com/mskian/deno-cricket-cli/actions/workflows/test.yml/badge.svg)](https://github.com/mskian/deno-cricket-cli/actions/workflows/test.yml)
+![Deno](https://img.shields.io/badge/Deno-464647?style=for-the-badge&logo=deno&logoColor=white)
+
+Live Cricket Score CLI - Get Live Cricket Score Update on your Terminal and
+CMD - Build using Deno.
+
+## Prerequisites
+
+- Deno on your System <https://deno.land/#installation>
+
+## Features
+
+- Get Current Live Cricket Match Score
+- Get Live Match Score by Entering Cricbuzz Live Match Score ID (You can Get it
+  from URL)
+
+## Usage
+
+- Clone or Download the Repo
+
+```sh
+git clone https://github.com/mskian/deno-cricket-cli.git
+cd deno-cricket-cli
+```
+
+- Run the CLI without installation
+
+```sh
+# Get Current Live Cricket Match Score
+deno run --allow-net --allow-read main.ts -l
+
+(or)
+
+deno task live
+
+# Get Live Match Score by Entering Cricbuzz Live Match Score ID (You can Get it from URL)
+deno run --allow-net --allow-read main.ts -m 123456
+
+(or)
+
+deno task match 123456
+
+# Help Section
+deno run --allow-net --allow-read main.ts -h
+
+(or)
+
+deno task help
+```
+
+- install via Deno Modules - <https://deno.land/x/cricket>
+- Learn about deno Script installer -
+  <https://deno.land/manual/tools/script_installer>
+
+```sh
+deno install --allow-net --name cricket https://deno.land/x/cricket/main.ts
+```
+
+- install CLI via Cloned repo or download (For Development)
+
+```sh
+deno install --allow-net --name cricket main.ts
+```
+
+```sh
+# Get Current Live Cricket Match Score
+cricket -l
+
+# Get Live Match Score by Entering Cricbuzz Live Match Score ID (You can Get it from URL)
+cricket -m 123456
+
+# Help Section
+cricket -h
+```
+
+## uninstall
+
+```sh
+deno uninstall cricket
+```
+
+- Use directly without download or install Locally by using `deno.land` or
+  `nest.land`
+
+```sh
+deno run --allow-net https://deno.land/x/cricket/main.ts -l
+deno run --allow-net https://deno.land/x/cricket/main.ts -m 123456
+deno run --allow-net https://deno.land/x/cricket/main.ts -h
+```
+
+```sh
+deno run --allow-net https://x.nest.land/deno-cricket-cli@0.0.1/main.ts -l
+deno run --allow-net https://x.nest.land/deno-cricket-cli@0.0.1/main.ts -m 123456
+deno run --allow-net https://x.nest.land/deno-cricket-clii@0.0.1/main.ts -h
+```
+
+## Credits
+
+- Build CLI Tool with Deno and Cliffy - <https://cliffy.io/>
+- Cricket API - <https://github.com/mskian/cricket-api-nodejs>
+- Cricket CLI Node.js - <https://github.com/mskian/cricket-cli>
+
+## LICENSE
+
+MIT
---
+++ main.ts
@@ -0,0 +1,197 @@
+import {
+  bold,
+  brightBlue,
+  brightGreen,
+  brightMagenta,
+  brightYellow,
+  gray,
+  red,
+} from "https://deno.land/std@0.150.0/fmt/colors.ts";
+import { wait } from "https://deno.land/x/wait@0.1.12/mod.ts";
+import { Command } from "https://deno.land/x/cliffy@v0.24.3/command/mod.ts";
+
+// deno-lint-ignore no-inferrable-types
+const BASE_URL: string = "https://cricket-api.vercel.app/";
+//const parsedArgs = parse(args);
+const pre = brightGreen(bold("›"));
+
+await new Command()
+  .name("cricket")
+  .description(
+    "Live Cricket Score CLI - Get Live Cricket Score Update on your Terminal and CMD.",
+  )
+  .version("v0.0.1")
+  .meta("deno", Deno.version.deno)
+  .meta("v8", Deno.version.v8)
+  .meta("typescript", Deno.version.typescript)
+  .example(
+    "Get Live Cricket Match Score from ID",
+    `cricket -m 123456`,
+  )
+  .option("-i, --info", "About Cricket CLI", { required: true })
+  .allowEmpty()
+  .action(function ({ info }) {
+    if (!info) {
+      this.showHelp();
+      return;
+    }
+    console.log(gray(bold(`+-+-+-+-+-+-+-+ +-+-+-+-+-+
+|C|r|i|c|k|e|t| |S|c|o|r|e|
++-+-+-+-+-+-+-+ +-+-+-+-+-+                                           
+Live Cricket Score CLI - Get Live Cricket Score Update on your Terminal and CMD\nCricket Score API - https://github.com/mskian/cricket-api-nodejs\nBuild and Developed by Santhosh Veer`)));
+  })
+  .option("-l, --live", "Get Current Live Cricket Match Score", {
+    standalone: true,
+    //required: true,
+    action: async () => {
+      const spinner = wait("Fetching Score Data...").start();
+      const res = await fetch(`${BASE_URL}live`);
+      const data = await res.json();
+      if (data.current !== "Data Not Found") {
+        spinner.stop();
+        console.log(`\n${pre}`, brightMagenta(bold(data.title)));
+        console.log(`\n${pre}`, brightYellow(bold(data.update)));
+        console.log(
+          `\n${pre}`,
+          brightMagenta(bold("Live Score:")),
+          brightGreen(bold(data.current)),
+        );
+        console.log(
+          `\n${pre}`,
+          brightMagenta(bold("Run Rate:")),
+          brightBlue(bold(data.runrate)),
+        );
+        console.log(
+          `\n${pre}`,
+          brightMagenta(bold("Current Batsman:")),
+          brightGreen(bold(data.batsman)),
+          brightMagenta(bold("Runs")),
+          brightGreen(bold(data.batsmanrun)),
+          "-",
+          brightGreen(bold(data.ballsfaced)),
+          brightMagenta(bold("balls")),
+        );
+        console.log(
+          `\n${pre}`,
+          brightMagenta(bold("Current Bowler:")),
+          brightGreen(bold(data.bowler)),
+          brightGreen(bold(data.bowlerover)),
+          brightMagenta(bold("over")),
+          brightGreen(bold(data.bowlerruns)),
+          brightMagenta(bold("run and")),
+          brightGreen(bold(data.bowlerwickets)),
+          brightMagenta(bold("wicket")),
+        );
+        console.log(
+          `\n${pre}`,
+          brightMagenta(bold("Other Batsman:")),
+          brightGreen(bold(data.batsmantwo)),
+          brightMagenta(bold("Runs")),
+          brightGreen(bold(data.batsmantworun)),
+          "-",
+          brightGreen(bold(data.batsmantwoballsfaced)),
+          brightMagenta(bold("balls")),
+        );
+        console.log(
+          `\n${pre}`,
+          brightMagenta(bold("Other Bowler:")),
+          brightGreen(bold(data.bowlertwo)),
+          brightGreen(bold(data.bowletworover)),
+          brightMagenta(bold("over")),
+          brightGreen(bold(data.bowlertworuns)),
+          brightMagenta(bold("run and")),
+          brightGreen(bold(data.bowlertwowickets)),
+          brightMagenta(bold("wicket")),
+        );
+        console.log("\n");
+      } else {
+        spinner.stop();
+        const errorMsg = red(
+          `${red(bold("Sorry Currently No Live Match"))}`,
+        );
+        console.log(errorMsg);
+      }
+    },
+  })
+  .option(
+    "-m, --match <matchID>",
+    "Get Live Match Score by Entering Cricbuzz Live Match Score ID (You can Get it from URL)",
+    {
+      standalone: true,
+      action: async ({ match }) => {
+        const spinner = wait("Fetching Score Data...").start();
+        const getMatchNumber = match;
+        if (getMatchNumber) {
+          const res = await fetch(
+            `${BASE_URL}score?url=https://m.cricbuzz.com/live-cricket-scores/${getMatchNumber}`,
+          );
+          const data = await res.json();
+          if (data.current !== "Data Not Found") {
+            spinner.stop();
+            console.log(`\n${pre}`, brightMagenta(bold(data.title)));
+            console.log(`\n${pre}`, brightYellow(bold(data.update)));
+            console.log(
+              `\n${pre}`,
+              brightMagenta(bold("Live Score:")),
+              brightGreen(bold(data.current)),
+            );
+            console.log(
+              `\n${pre}`,
+              brightMagenta(bold("Run Rate:")),
+              brightBlue(bold(data.runrate)),
+            );
+            console.log(
+              `\n${pre}`,
+              brightMagenta(bold("Current Batsman:")),
+              brightGreen(bold(data.batsman)),
+              brightMagenta(bold("Runs")),
+              brightGreen(bold(data.batsmanrun)),
+              "-",
+              brightGreen(bold(data.ballsfaced)),
+              brightMagenta(bold("balls")),
+            );
+            console.log(
+              `\n${pre}`,
+              brightMagenta(bold("Current Bowler:")),
+              brightGreen(bold(data.bowler)),
+              brightGreen(bold(data.bowlerover)),
+              brightMagenta(bold("over")),
+              brightGreen(bold(data.bowlerruns)),
+              brightMagenta(bold("run and")),
+              brightGreen(bold(data.bowlerwickets)),
+              brightMagenta(bold("wicket")),
+            );
+            console.log(
+              `\n${pre}`,
+              brightMagenta(bold("Other Batsman:")),
+              brightGreen(bold(data.batsmantwo)),
+              brightMagenta(bold("Runs")),
+              brightGreen(bold(data.batsmantworun)),
+              "-",
+              brightGreen(bold(data.batsmantwoballsfaced)),
+              brightMagenta(bold("balls")),
+            );
+            console.log(
+              `\n${pre}`,
+              brightMagenta(bold("Other Bowler:")),
+              brightGreen(bold(data.bowlertwo)),
+              brightGreen(bold(data.bowletworover)),
+              brightMagenta(bold("over")),
+              brightGreen(bold(data.bowlertworuns)),
+              brightMagenta(bold("run and")),
+              brightGreen(bold(data.bowlertwowickets)),
+              brightMagenta(bold("wicket")),
+            );
+            console.log("\n");
+          } else {
+            spinner.stop();
+            const errorMsg = red(
+              `${red(bold("Sorry Currently No Live Match"))}`,
+            );
+            console.log(errorMsg);
+          }
+        }
+      },
+    },
+  )
+  .parse();
